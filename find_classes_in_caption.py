5e20c66967c1e43ab6025ce7ebd64c521f217747
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-09-11 17:03:22 +0300

    Changed stopsign to sign and added sidewalk class

--- find_classes_in_caption.py
+++ find_classes_in_caption.py
@@ -3,14 +3,14 @@ from nltk.corpus import wordnet as wn
 
 word_classes = [
     'man', 'woman', 'boy', 'girl', 'person', 'people', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',
-    'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+    'boat', 'traffic light', 'fire hydrant', 'sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
     'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag',
     'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'ball', 'kite', 'baseball bat', 'baseball glove',
     'skateboard', 'surfboard', 'tennis racket', 'bottle', 'glass', 'cup', 'fork', 'knife', 'spoon',
     'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
     'cake', 'chair', 'couch', 'plant', 'bed', 'table', 'toilet', 'television', 'laptop', 'mouse', 'remote',
     'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
-    'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush', 'wall'
+    'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush', 'wall', 'sidewalk'
     ]
 
 known_mappings = {
