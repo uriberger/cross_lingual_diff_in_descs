fb53a9fc530b13df96e66bc0b56012d3890347b2
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-09-11 15:55:31 +0300

    Added code to find classes

--- find_classes_in_caption.py
+++ find_classes_in_caption.py
@@ -43,13 +43,7 @@ def find_word_classes(synset):
             classes += find_word_classes(hypernym)
         return list(set(classes))
     
-def extract_noun_spans(caption):
-    doc = nlp(caption)
-    token_lists = [[x.to_dict() for x in y.tokens] for y in doc.sentences]
-    if len(token_lists) > 1:
-        return None
-    token_list = token_lists[0]
-
+def extract_noun_spans(token_list):
     noun_spans = []
 
     # First find sequences of nouns
@@ -88,31 +82,25 @@ def extract_noun_spans(caption):
     return noun_spans
 
 def find_classes(caption):
-    noun_spans = extract_noun_spans(caption)
+    doc = nlp(caption)
+    token_lists = [[x.to_dict() for x in y.tokens] for y in doc.sentences]
+    if len(token_lists) > 1:
+        return None
+    token_list = token_lists[0]
+
+    noun_spans = extract_noun_spans(token_list)
     classes = []
 
-    # TODO
-    # for start_ind, end_ind in noun_spans:
-    #     # Find the lowest noun
-    #     lowest_noun_depth = max(depths) + 1
-    #     lowest_noun_ind = -1
-    #     for cur_ind in range(start_ind, end_ind):
-    #         if depths[cur_ind] < lowest_noun_depth and token_list[cur_ind][0]['upos'] == 'NOUN':
-    #             lowest_noun_depth = depths[cur_ind]
-    #             lowest_noun_ind = cur_ind
-
-    #     if lowest_noun_ind != -1:
-    #         cur_word = token_list[lowest_noun_ind][0]['text']
-    #         synsets = wn.synsets(cur_word)
-    #         cur_classes = []
-    #         for synset in synsets:
-    #             cur_classes += find_word_classes(synset)
-    #         if len(cur_classes) == 0:
-    #             cur_class = None
-    #         assert len(cur_classes) == 1, f'Word {cur_word} has multiple classes'
-    #         cur_class = cur_classes[0]
-    #     else:
-    #         cur_class = None
-    #     classes.append(cur_class)
+    for start_ind, end_ind in noun_spans:
+        pharse = ' '.join([token_list[i][0]['text'] for i in range(start_ind, end_ind)])
+        synsets = wn.synsets(pharse)
+        cur_classes = []
+        for synset in synsets:
+            cur_classes += find_word_classes(synset)
+        if len(cur_classes) == 0:
+            cur_class = None
+        assert len(cur_classes) == 1, f'Word {pharse} has multiple classes'
+        cur_class = cur_classes[0]
+        classes.append((start_ind, end_ind, cur_class))
     
     return classes
