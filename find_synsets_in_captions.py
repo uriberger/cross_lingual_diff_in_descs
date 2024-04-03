7eb6e5c2abfe487e5f42845b8524ad7267ded6f7
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-04-03 17:01:22 +1100

    Some more improvements to water handling

--- find_synsets_in_captions.py
+++ find_synsets_in_captions.py
@@ -388,12 +388,16 @@ def water_handling(token_list, start_ind):
     if start_ind > 1 and token_list[start_ind - 2][0]['text'] in ['pool', 'pools', 'pond', 'ponds'] and token_list[start_ind - 1][0]['text'] == 'of':
         return [('body_of_water.n.01', 0)]
     
-    # Bottle/bottles is food
-    if start_ind > 1 and token_list[start_ind - 2][0]['text'] in ['bottle', 'bottles'] and token_list[start_ind - 1][0]['text'] == 'of':
+    # Bottle/glass is food
+    if start_ind > 1 and token_list[start_ind - 2][0]['text'] in ['bottle', 'bottles', 'glass', 'glasses'] and token_list[start_ind - 1][0]['text'] == 'of':
         return [('water.n.06', 0)]
     
-    # Source is body of water
-    if start_ind < len(token_list) - 1 and token_list[start_ind + 1][0]['text'] == 'source':
+    # Some verbs is food
+    if start_ind > 0 and token_list[start_ind - 1][0]['text'] == ['steaming', 'drinking']:
+        return [('water.n.06', 0)]
+    
+    # Some post words is body of water
+    if start_ind < len(token_list) - 1 and token_list[start_ind + 1][0]['text'] == ['source', 'fountain']:
         return [('body_of_water.n.01', 0)]
     
     # Some adjectives always mean the body of water meaning
