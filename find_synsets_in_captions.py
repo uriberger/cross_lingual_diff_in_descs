afa5fa0f86dddd40bfec39281fa321586d22acd0
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-04-15 13:04:41 +1000

    Improved water handling

--- find_synsets_in_captions.py
+++ find_synsets_in_captions.py
@@ -378,7 +378,7 @@ def water_handling(token_list, start_ind):
         return [('water.n.06', 0)]
     
      # Some post words is food
-    if start_ind < len(token_list) - 1 and token_list[start_ind + 1][0]['text'] in ['bottle', 'glass']:
+    if start_ind < len(token_list) - 1 and token_list[start_ind + 1][0]['text'] in ['bottle', 'glass', 'bottles', 'glasses']:
         return [('water.n.06', 0)]
     
     # Some verbs is food
