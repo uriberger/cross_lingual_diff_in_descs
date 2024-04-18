2b52adc3203da52e037aef109856e39567a3bdb2
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-04-18 10:31:45 +1000

    Added hand handling

--- find_synsets_in_captions.py
+++ find_synsets_in_captions.py
@@ -444,6 +444,7 @@ single_word_to_handling_func = {
     'mouse': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['computer'], [(None, 0)], [('mouse.n.01', 0), (None, 0)]),
     'player': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['audio', 'music'], [(None, 0)], [('player.n.01', 0)]),
     'willow': lambda token_list, start_ind: succeeding_word_handling_func(token_list, start_ind, ['house'], [(None, 0)], [('tree.n.01', 1)]),
+    'hand': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['second'], [(None, 0)], [('hand.n.01', 0)]),
 }
 
 def phrase_location_to_synset(token_list, start_ind, end_ind):
