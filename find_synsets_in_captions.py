4a5350765551f1bb4f8e7062e622a96ef9ea621a
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-06-05 12:10:49 +1000

    Improved player handling

--- find_synsets_in_captions.py
+++ find_synsets_in_captions.py
@@ -473,7 +473,7 @@ single_word_to_handling_func = {
     'mouse': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['computer'], [(None, 0)], [('mouse.n.01', 0), (None, 0)]),
     'processor': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['food'], [(None, 0)], [('electronic_equipment.n.01', 1)]),
     'processors': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['food'], [(None, 0)], [('electronic_equipment.n.01', 1)]),
-    'player': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['audio', 'music', 'record', 'media'], [(None, 0)], [('player.n.01', 0)]),
+    'player': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['audio', 'music', 'record', 'media', 'digital'], [(None, 0)], [('player.n.01', 0)]),
     'willow': lambda token_list, start_ind: succeeding_word_handling_func(token_list, start_ind, ['house'], [(None, 0)], [('tree.n.01', 1)]),
     'hand': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['second'], [(None, 0)], [('hand.n.01', 0)]),
     'plant': lambda token_list, start_ind: preceding_word_handling_func(token_list, start_ind, ['power', 'industrial'], [('factory.n.01', 0)], [('plant.n.02', 0)]),
