3a1cec868295602dfc7a26408994624eecef4104
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-06-05 11:34:26 +1000

    some more improvements to boejct num analysis

--- analysis_utils.py
+++ analysis_utils.py
@@ -388,14 +388,14 @@ def plot_object_num(langs, synset_list, by_location):
             res = get_object_num_by_language(langs, synset_list[i])
         if len(synset_list) == 1:
             axs[col].barh(range(36), width=[float(x[1]) for x in res], color=['red' if x[0] in east_asian_langs else 'black' for x in res])
-            axs[col].set_title(synset_list[i])
-            axs[col].set_yticks(ticks=range(36), labels=[x[0] for x in res])
-            axs[col].tick_params(axis='both', labelsize=10)
+            axs[col].set_title(synset_list[i], size=22, family='Times New Roman')
+            axs[col].set_yticks(ticks=range(36), labels=[x[0] for x in res], size=15, family='Times New Roman')
+            axs[col].tick_params(axis='x', labelsize=22, labelfontfamily='Times New Roman')
         else:
             axs[row, col].barh(range(36), width=[float(x[1]) for x in res], color=['red' if x[0] in east_asian_langs else 'black' for x in res])
-            axs[row, col].set_title(synset_list[i])
-            axs[row, col].set_yticks(ticks=range(36), labels=[x[0] for x in res])
-            axs[row, col].tick_params(axis='both', labelsize=10)
+            axs[row, col].set_title(synset_list[i], size=22, family='Times New Roman')
+            axs[row, col].set_yticks(ticks=range(36), labels=[x[0] for x in res], size=15, family='Times New Roman')
+            axs[row, col].tick_params(axis='x', labelsize=22, labelfontfamily='Times New Roman')
         col += 1
         if col == col_num:
             col = 0
